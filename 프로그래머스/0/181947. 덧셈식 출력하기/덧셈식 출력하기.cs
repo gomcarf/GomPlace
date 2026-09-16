a4343d45bd9a75e90c347523f74ac60e6b76ac49
gomcarf <33718488+gomcarf@users.noreply.github.com>
2026-09-17 00:25:43 +0900

    [level 0] Title: 덧셈식 출력하기, Time: 24.19 ms, Memory: 30.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181947. 덧셈식 출력하기/덧셈식 출력하기.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Example
+{
+    public static void Main()
+    {
+        String[] s;
+
+        s = Console.ReadLine().Split(' ');
+
+        int a = Int32.Parse(s[0]);
+        int b = Int32.Parse(s[1]);
+
+        Console.WriteLine($"{a} + {b} = {a+b}");
+    }
+}
