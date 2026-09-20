6633b0baa23220645a00758876eb5d633c842fab
gomcarf <33718488+gomcarf@users.noreply.github.com>
2026-09-20 23:41:51 +0900

    Update 홀짝에 따라 다른 값 반환하기.cs

--- Prgrammers/0/181935/홀짝에 따라 다른 값 반환하기.cs
+++ Prgrammers/0/181935/홀짝에 따라 다른 값 반환하기.cs
@@ -9,3 +9,14 @@ public class Solution {
             return 2*m*(m+1)*(2*m+1)/3;
     }
 }
+/*
+삼항 연산자 사용한 풀이
+n이 짝수라면 n이 0 이상인 동안 n*n을 더해가며 2씩 감소
+n이 짝수라면 n이 0 이상인 동안 n을 더해가며 2씩 감소
+while(n >=0)
+{
+    answer += n%2 == 0 ? n*n : n;            
+    n -= 2;
+}
+return answer;
+*/
