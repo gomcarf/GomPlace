ad07322d592691fea87d4f41fe6e5daed64825c8
gomcarf <33718488+gomcarf@users.noreply.github.com>
2026-09-21 23:38:28 +0900

    [level 0] Title: 조건 문자열, Time: 0.16 ms, Memory: 46.1 MB -BaekjoonHub

--- Prgrammers/0/181934/조건 문자열.cs
+++ Prgrammers/0/181934/조건 문자열.cs
@@ -2,7 +2,6 @@ using System;
 
 public class Solution {
     public int solution(string ineq, string eq, int n, int m) {
-        int answer = 0;
         if(ineq == ">"){
             if(eq == "="){
                 if(n>=m)
