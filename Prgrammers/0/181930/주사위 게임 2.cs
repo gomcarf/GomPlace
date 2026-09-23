7fc17485373c205feb287c0f206c85b2da6281a2
gomcarf <33718488+gomcarf@users.noreply.github.com>
2026-09-23 15:07:18 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.12 ms, Memory: 45.7 MB -BaekjoonHub

---
+++ Prgrammers/0/181930/주사위 게임 2.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Solution {
+    public int solution(int a, int b, int c) {
+        if(a==b && b==c)
+            return (a+b+c)*(a*a+b*b+c*c)*(a*a*a+b*b*b+c*c*c);
+        if(a!=b && b!=c && c!=a)
+            return a+b+c;
+        else
+            return (a+b+c)*(a*a+b*b+c*c);
+    }
+}
