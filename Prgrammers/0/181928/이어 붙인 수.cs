e0e0f08a5ec032ccd215fec6fdd22ea5fc9df62a
gomcarf <33718488+gomcarf@users.noreply.github.com>
2026-09-25 08:49:52 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.83 ms, Memory: 47.1 MB -BaekjoonHub

---
+++ Prgrammers/0/181928/이어 붙인 수.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        string odd = "";
+        string even = "";
+        for(int i = 0; i < num_list.Length; i++){
+            if(num_list[i] % 2 == 0)
+                even += num_list[i].ToString();
+            else
+                odd += num_list[i].ToString();
+        }
+        answer = int.Parse(even) + int.Parse(odd);
+        return answer;
+    }
+}
