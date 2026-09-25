a30ffc657d02400a36233ef5472730ff0303f92d
gomcarf <33718488+gomcarf@users.noreply.github.com>
2026-09-25 19:53:26 +0900

    [level 0] Title: 수 조작하기 2, Time: 1156.01 ms, Memory: 207 MB -BaekjoonHub

---
+++ Prgrammers/0/181925/수 조작하기 2.cs
@@ -0,0 +1,28 @@
+using System;
+
+public class Solution {
+    public string solution(int[] numLog) {
+        string answer = "";
+        for(int i = 1; i < numLog.Length; i++)
+        {
+            int a = numLog[i] - numLog[i-1];
+            switch(a)
+            {
+                case 1 :
+                    answer += "w";
+                    break;
+                case -1 :
+                    answer += "s";
+                    break;
+                case 10:
+                    answer += "d";
+                    break;
+                case -10:
+                    answer += "a";
+                    break;
+            }
+        }
+        return answer;
+        return answer;
+    }
+}
