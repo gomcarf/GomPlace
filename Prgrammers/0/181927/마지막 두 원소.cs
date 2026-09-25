b263ab2bbb1433c11d3efba7deb84b73e2083531
gomcarf <33718488+gomcarf@users.noreply.github.com>
2026-09-25 09:41:15 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.13 ms, Memory: 45.8 MB -BaekjoonHub

--- Prgrammers/0/181927/마지막 두 원소.cs
+++ Prgrammers/0/181927/마지막 두 원소.cs
@@ -1,12 +1,10 @@
 using System;
-using System.Collections.Generic;
 
 public class Solution {
     public int[] solution(int[] num_list) {
         int[] answer = new int[num_list.Length+1];
         
-        for(int i = 0; i<num_list.Length; i++)
-            answer[i] = num_list[i];
+        Array.Copy(num_list, answer, num_list.Length);
         
         int last = num_list[num_list.Length - 1];
         int prev = num_list[num_list.Length - 2];
